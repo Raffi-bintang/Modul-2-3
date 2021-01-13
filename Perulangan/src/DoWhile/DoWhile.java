33f1366f6112f3f3b89f27bdb078def1cd0a5f0d
Raffi-bintang <73929392+Raffi-bintang@users.noreply.github.com>
2021-01-13 07:41:09 +0700

    Perulangan

---
+++ Perulangan/src/DoWhile/DoWhile.java
@@ -0,0 +1,17 @@
+
+package DoWhile;
+
+
+public class DoWhile {
+
+    
+    public static void main(String[] args) {
+        int value = 1;
+        do {
+            System.out.println("Angka : " + value);
+            value++;
+        } while (value <= 10);
+    }
+    }
+    
+
---
+++ Perulangan/src/FOR/FOR.java
@@ -0,0 +1,14 @@
+
+package FOR;
+
+
+public class FOR {
+
+   
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Angka : " + i);
+        }
+    }
+    
+}
