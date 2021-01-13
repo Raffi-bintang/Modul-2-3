0281cff9ad5af64d6258accab475e1649186de95
Raffi-bintang <73929392+Raffi-bintang@users.noreply.github.com>
2021-01-13 07:44:21 +0700

    Class

---
+++ Class/src/KucingAnggora/KucingAnggora.java
@@ -0,0 +1,14 @@
+
+package KucingAnggora;
+
+
+public class KucingAnggora {
+    String namaKucing;
+    int beratBadan;
+ 
+   
+    public KucingAnggora(String nama, int berat) {
+        namaKucing = nama;
+        beratBadan = berat;
+    }
+}
