480b8cf8cc459878c4aacd686c743f2b97517fcd
Raffi-bintang <73929392+Raffi-bintang@users.noreply.github.com>
2021-01-13 07:53:26 +0700

    Properti dan Metode

---
+++ Function/src/CallFunction/callfunction.java
@@ -0,0 +1,17 @@
+
+package CallFunction;
+
+
+public class callfunction {
+
+    
+    public static void main(String[] args) {
+          cobaFungsi();
+    }
+ 
+    public static void cobaFungsi() {
+        System.out.println("Ini merupakan bagian dari fungsi");
+    }
+    
+    
+}
